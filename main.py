0317b281bfb2b05ad61d5804ef9e6fbe34ada615
Oluwapelumi Bamidele <oluwapelumibamidele001@gmail.com>
2026-09-18 10:44:27 +0100

    chore: initialize main.py and set up basic input handling

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+CampusCart CLI Application
+Author: Bamidele Oluwapelumi Adeleke
+Description: An interactive procedural CLI tools for managing a CampusCart using variables,
+    conditional loops, dictionaries, and list structures.
+"""
+
+# Display Welcome Banner and input
+print("=" * 40)
+print("     WELCOME TO CAMPUSCART CLI SYSTEM     ")
+print("=" * 40)
+
+user_name = input("Enter your name to start: ").strip().title()
+print(f"\nHello, {user_name}! System initialized successfully.\n")
+
