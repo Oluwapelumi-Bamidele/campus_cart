ea31e40acc1e67243a1b5dbd158eb3ca4a29a45a
Oluwapelumi Bamidele <oluwapelumibamidele001@gmail.com>
2026-09-19 12:48:28 +0100

    feat: added inventory dictionary and cart list

--- main.py
+++ main.py
@@ -13,3 +13,26 @@ print("=" * 40)
 user_name = input("Enter your name to start: ").strip().title()
 print(f"\nHello, {user_name}! System initialized successfully.\n")
 
+#phase 2: data structures design
+#catalogue dictionary structure and active shooping cart list
+inventory = {
+    "101": {"name": "40 leaves notebook", "price": 2.50, "stock": 15},
+    "102": {"name": "80 leaves notebook", "price": 4.50, "stock": 15},
+    "103": {"name": "Pen Packs", "price": 5, "stock": 10},
+    "104": {"name": "school bags", "price": 7.5, "stock": 5},
+    "105": {"name": "Food Flasks", "price": 10, "stock": 3},
+    "106": {"name": "school uniform", "price": 10, "stock": 23},
+    "107": {"name": "lamp", "price": 2.7, "stock": 25},
+    "108": {"name": "shoe", "price": 8, "stock": 15},
+    "109": {"name": "water bottle", "price": 4.9, "stock": 15},
+    "110": {"name": "laptop", "price": 50, "stock": 30}
+    }
+
+cart = []
+
+print("\n--- Goods ready ---")
+print(f"Catalogue loaded with {len(inventory)} products.")
+print(f"initial cart state: {cart}")
+
+
+
